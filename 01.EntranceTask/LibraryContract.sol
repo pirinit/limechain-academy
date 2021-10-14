3aae107efcf09247505d650f08666a2771a4d5b9
Pirin Karabenchev <pirin.karabenchev@gmail.com>
2021-10-15 00:07:45 +0300

    Ownbasic Ownable.sol contract added, syntax fixes

--- 01.EntranceTask/LibraryContract.sol
+++ 01.EntranceTask/LibraryContract.sol
@@ -1,12 +1,16 @@
-contract Library {
+pragma solidity ^0.8.0;
+
+import "./Ownable.sol";
+
+contract Library is Ownable {
 	struct Book {
-		uint32 id,
-		uint16 totalCopies,
-		uint16 freeCopies,
-		string name
+		uint32 id;
+		uint16 totalCopies;
+		uint16 freeCopies;
+		string name;
 	}
 	
-	Book[] books private;
+	Book[] private books;
 	
 	//mapping for borrowers - current
 	//mapping for borrowers - all time/history
@@ -17,21 +21,21 @@ contract Library {
 		// create and add new book instance
 	}
 	
-	function listAllBooks() public view returns(Book[]) {
+	function listAllBooks() public view returns(Book[] memory) {
 		//what about paging? how big "responses" can we have?
 		//filter out books with no freeCopies - or maybe have a paramater do we want to see them?
 		
 	}
 	
-	function borrowBook(uint32 calldata _bookId) public {
+	function borrowBook(uint32 _bookId) public {
 		// check if current user has borrowed this particular book
 		// check if there are free books to borrow
 	}
 	
-	function returnBook(uint32 calldata _bookId) public {
+	function returnBook(uint32 _bookId) public {
 		// check if current user has borrowed this particular book
 	}
 	
-	function listBorrowers(uint32 calldata _bookId) public view returns(address[]) {
+	function listBorrowers(uint32 _bookId) public view returns(address[] memory) {
 	}
 }
